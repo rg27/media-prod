9f186a5de07a851d51df29aa7d790830af961efd
rg27 <gualvezreymar@gmail.com>
2021-03-08 01:27:54 +0800

    Modifying pages

--- pages/snippets/[slug].js
+++ pages/snippets/[slug].js
@@ -1,32 +1,32 @@
-import hydrate from 'next-mdx-remote/hydrate';
+// import hydrate from 'next-mdx-remote/hydrate';
 
-import { getFiles, getFileBySlug } from '@/lib/mdx';
-import SnippetLayout from '@/layouts/snippets';
-import MDXComponents from '@/components/MDXComponents';
+// import { getFiles, getFileBySlug } from '@/lib/mdx';
+// import SnippetLayout from '@/layouts/snippets';
+// import MDXComponents from '@/components/MDXComponents';
 
-export default function Snippet({ mdxSource, frontMatter }) {
-  const content = hydrate(mdxSource, {
-    components: MDXComponents
-  });
+// export default function Snippet({ mdxSource, frontMatter }) {
+//   const content = hydrate(mdxSource, {
+//     components: MDXComponents
+//   });
 
-  return <SnippetLayout frontMatter={frontMatter}>{content}</SnippetLayout>;
-}
+//   return <SnippetLayout frontMatter={frontMatter}>{content}</SnippetLayout>;
+// }
 
-export async function getStaticPaths() {
-  const snippets = await getFiles('snippets');
+// export async function getStaticPaths() {
+//   const snippets = await getFiles('snippets');
 
-  return {
-    paths: snippets.map((s) => ({
-      params: {
-        slug: s.replace(/\.mdx/, '')
-      }
-    })),
-    fallback: false
-  };
-}
+//   return {
+//     paths: snippets.map((s) => ({
+//       params: {
+//         slug: s.replace(/\.mdx/, '')
+//       }
+//     })),
+//     fallback: false
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const snippet = await getFileBySlug('snippets', params.slug);
+// export async function getStaticProps({ params }) {
+//   const snippet = await getFileBySlug('snippets', params.slug);
 
-  return { props: snippet };
-}
+//   return { props: snippet };
+// }
